806907ae02f6a89bef712d2d6c25e500783ea96d
gayathri828 <gayathrisreeja49@gmail.com>
2025-07-18 19:09:32 +0530

    Initial commit - Production suits Django project

--- production_suits/production_suits/urls.py
+++ production_suits/production_suits/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('suits/', include('suits.urls')),
+    path('', lambda request: redirect('/suits/register/')),  # Redirect root to register page
 ]
